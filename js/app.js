098bc0cb49cfc02c12250a7307d94c9fc70d2908
Ahmed Mohamed Gaber <ahmed.mohamed.gaber@gmail.com>
2018-03-30 23:58:20 +0200

    stop watch added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-$( document ).ready(function pageScript(){/*
+$( document ).ready(function pageScript(){
+/*
  * Create a list that holds all of your cards
  */
 
@@ -49,128 +50,80 @@ $(this).toggleClass(class_array[index]);
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-
-
-
-
-
-
+var matches_count=0;
 function controlclass(card_counter, clicked_cards, chosen_card){
-
-
-
-
-
-
-    switch(card_counter) {
-
+    switch(card_counter){
         case 0:
-          /*  $(".open").removeClass("open show");
-            $(chosen_card).addClass('match');*/
-            
-
-
-            console.log("case 0 ");
-            console.log("clicked_cards[0]= " + clicked_cards[0])
-            console.log("open added");
-            break;
+        break;
         case 1:
-            console.log("case 1");
-
             if(clicked_cards[0] === clicked_cards[1] && clicked_cards[card_counter] !== null){
-
                 $(chosen_card).addClass('match');
                 $("."+clicked_cards[0]).parent().addClass("match");
                 $(".open").removeClass("open show");
+                matches_count +=2;
             }else{
-                /*$(chosen_card).toggleClass("open show");
-                $("."+clicked_cards[0]).parent().removeClass("open show");*/
-               /* var open_items = 0;
-                $(".open").each(
-                    function(index){
-                        for(i=0;i<index;i++){
-                            open_items++;
-                        }
-                        if(open_items)
-                    $(this).toggleClass(class_array[index]);
-                    });*/
                 $(".open").removeClass("open show");
-
             }
+   }
+    if(matches_count === 16){
+        welldone();
 
-            break;
+}
 
+}
+function welldone(){
+
+    time_watch(false);      
 
 }
 
 
+var seconds =0;
+var increment_seconds = null;
+function time_watch(true_false){
+
+    
+
+                 
+      
+
+    if(true_false){
+        increment_seconds = setInterval(function(){ 
+        console.log("started time");
+         seconds++;
+        $(".seconds").html(seconds);},1000);  
+      
+    }else{
+ console.log("all done");
+ clearInterval(increment_seconds);
+ console.log("stop_timing called");
+ return;
+    }
+                   
+      
 
 }
-   var clicks =0;
+             
+var clicks=0;
 
 function count_clicks(){
    clicks++;
-   console.log(clicks);
-   $(".moves").html(clicks) ;
-   console.log("num clcks"+clicks);
-
-   if(clicks>16&&clicks<26){
-
-$(".stars li:nth-child(3) ").css("display" ,"none");
-   }else if(clicks>=26&&clicks<32){
-    console.log("26");
-    $(".stars li:nth-child(2) ").css("display" ,"none");
-
-   }else if(clicks >=32){
+    $(".moves").html(clicks);
+    if(clicks === 1){
+    time_watch(true);
+    }else if(clicks>16&&clicks<26){
+        $(".stars li:nth-child(3) ").css("display" ,"none");
+    }else if(clicks>=26&&clicks<32){
+        $(".stars li:nth-child(2) ").css("display" ,"none");
+    }else if(clicks >=32){
         $(".stars li:nth-child(1) ").css("display" ,"none");
-
-   }
+    }
 }
-
-
 
 
 function show_cards(selected){
 
-/*var open_items = 0;
-$(".open").each(
-                    function(index){
-                        
-                            open_items++;
-                        
-                       
-                    });
-
-
-console.log("open items : " + open_items);
-
-if(open_items<2){
-                           */  $(selected).toggleClass("open show");
-/*clicked_cards[card_counter] =   $(selected).children[0].classList.item(2);*/
-
-                    /*     }else{
-
-                            return;
-                         }
-
-
-
-
-$(".open").each(
-                    function(index){
-                        for(i=0;i<index;i++){
-                            open_items++;
-                        }
-                        if(open_items < 1 ){
-                         $(selected).toggleClass("open show");
-                        }else{
-                            $(".open").removeClass("open show");
-                         $(selected).toggleClass("open show");
-                        }
-                    });
-                    */
-
+    $(selected).toggleClass("open show");
 
 }
 
@@ -178,74 +131,48 @@ $(".open").each(
 function count_open(){
 
     var open_items = 0;
-$(".open").each(
-                    function(index){
-                        
-                            open_items++;
-                        
-                       
-                    });
-
-
-console.log("open items : " + open_items);
-
-if(open_items<2){
-return true;
-
-                         }else{
-
-                            return false;
-                         }
-
+    $(".open").each(
+        function(index){
+            open_items++;   
+    });
+    if(open_items<2){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 var clicked_cards = new Array(2);
 var card_counter = 0;
 
 $(".card").click( function set_clicked(e){
-
-
-
-if(count_open() === false){
-    console.log(count_open());
-    return;
-}
-
-
-
-
-
-var chosen_card = e.currentTarget;
-clicked_cards[card_counter] = e.currentTarget.children[0].classList.item(2);
-
-
-
-
-
-if( $(chosen_card).hasClass('open') === false){
-    show_cards(chosen_card);
-
-  var  change_class_delayed = setTimeout(controlclass , 2000 , card_counter ,clicked_cards ,chosen_card);
-
-
-if(card_counter ===0){
-    card_counter++;
-}else if(card_counter === 1 ){
-    card_counter--;
-    change_class_delayed;
-}
-console.log(card_counter + clicked_cards[card_counter]);
-}else{
-  $(this).removeClass("open show");
+    count_clicks();
+    if(count_open() === false){
+        return;
+    }
+    var chosen_card = e.currentTarget;
+    clicked_cards[card_counter] = e.currentTarget.children[0].classList.item(2);
+    if( $(chosen_card).hasClass('open') === false){
+        show_cards(chosen_card);
+     setTimeout(controlclass , 750 , card_counter ,clicked_cards ,chosen_card);
+    if(card_counter ===0){
+        card_counter++;
+    }else if(card_counter === 1 ){
+        card_counter--;
+    }
+    }else{
+        $(this).removeClass("open show");
         clicked_cards[card_counter] = null;
-
-  if(card_counter ===1){
-    card_counter-- ;
-  }
-}
-
-
+        if(card_counter ===1){
+            card_counter-- ;
+        }
+    }
 });
 
+
+$(".restart").click(function restart_page(){
+
+     location.reload(); 
+});
 
 });
